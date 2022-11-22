169f72d758a3a82a2cb01d7a56df2e21a48f0e44
GABRIEL RODRIGUES FARHAT <105125490+Follzer@users.noreply.github.com>
2022-11-22 19:21:09 -0300

    add paciente

--- JS/registro.js
+++ JS/registro.js
@@ -16,3 +16,30 @@ for (var i = 0; i < pacientes.length; i++) {
         paciente.style.backgroundColor = "rgba(255, 0, 0, 0.442)";
     };
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var sangue = form.sangue.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var tipoSangueTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    tipoSangueTd.textContent = sangue;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(tipoSangueTd);
+
+    var tabela = document.querySelector("#tabela-paciente");
+
+    tabela.appendChild(pacienteTr);
+
+}
+)
